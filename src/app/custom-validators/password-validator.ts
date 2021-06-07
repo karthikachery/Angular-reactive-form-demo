3dcf99c545666050e805c707d9cdec9792092e78
karthikachery <karthikachery@gmail.com>
2021-06-07 23:30:06 +0530

    Angular Reactive for Demo

---
+++ src/app/custom-validators/password-validator.ts
@@ -0,0 +1,16 @@
+import {FormGroup} from '@angular/forms'
+
+export function PasswordChecker(controlName:string,compareControlName:string){
+
+    return(formGroup:FormGroup) =>{
+        const password= formGroup.controls[controlName];
+        const confPassword= formGroup.controls[compareControlName];
+
+        if(confPassword !== password){
+            confPassword.setErrors({mustmatch:true});
+        }
+        else{
+            confPassword.setErrors(null);
+        }
+    }
+}
